0f6ca02c509b43097d6a00a928ec42578e30e4ef
Felix Wruck <felix.wruck@aisec.fraunhofer.de>
2023-11-02 16:51:05 +0100

    prepare_ws.sh: Adapt meta-tmedbg location
    This commit adapts prepare_ws.sh s.t. meta-tmedbg is searched relativ to
    the given build directory.

--- prepare_ws.sh
+++ prepare_ws.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ -z "$1" ];then
+BUILD_DIR="$1"
+
+if [ -z "BUILD_DIR" ];then
 	echo "No build directory given, exiting..."
 	exit 1
 fi
 
-(cd "$1" && bitbake-layers add-layer ../meta-tmedbg)
+(cd "$1" && bitbake-layers add-layer $BUILD_DIR/../meta-tmedbg)
 
-for f in $1/conf/local.conf $1/../meta-trustx/conf/multiconfig/*.conf;do
+for f in $BUILD_DIR/conf/local.conf $BUILD_DIR/../meta-trustx/conf/multiconfig/*.conf;do
 	echo "Patching DISTRO in $f"
 	sed -i 's/DISTRO = "gyroidos-core"/DISTRO = "cml-debug"/' $f
 	sed -i 's/DISTRO = "gyroidos-cml"/DISTRO = "cml-debug-tiny"/' $f
