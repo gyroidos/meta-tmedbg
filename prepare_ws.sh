d6a2a56a37d43bc6f65fccf611dfb58fd9bd0aef
Felix Wruck <felix.wruck@aisec.fraunhofer.de>
2023-11-03 08:14:32 +0100

    Correctly patch distro in multiconfigs

--- prepare_ws.sh
+++ prepare_ws.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BUILD_DIR="$1"
 
@@ -9,7 +9,7 @@ fi
 
 (cd "$1" && bitbake-layers add-layer $BUILD_DIR/../meta-tmedbg)
 
-for f in $BUILD_DIR/conf/local.conf $BUILD_DIR/../meta-trustx/conf/multiconfig/*.conf;do
+for f in  $BUILD_DIR/conf/local.conf $BUILD_DIR/../{meta-trustx,meta-trustx-intel}/conf/multiconfig/*.{conf,inc};do
 	echo "Patching DISTRO in $f"
 	sed -i 's/DISTRO = "gyroidos-core"/DISTRO = "cml-debug"/' $f
 	sed -i 's/DISTRO = "gyroidos-cml"/DISTRO = "cml-debug-tiny"/' $f
