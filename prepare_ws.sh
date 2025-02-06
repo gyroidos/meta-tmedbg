f32a924a85958cc6eccca2a4d4d14dba4fc42e42
Michael Weiß <michael.weiss@aisec.fraunhofer.de>
2025-02-06 08:07:00 +0100

    prepare_ws: meta-trustx* is now meta-gyroidos*
    Since the upstream repo names changed, use the new names
    meta-gyrodios* here, too.
    
    Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

--- prepare_ws.sh
+++ prepare_ws.sh
@@ -11,7 +11,7 @@ fi
 
 (cd "$1" && bitbake-layers add-layer $BUILD_DIR/../meta-tmedbg)
 
-for f in  $BUILD_DIR/conf/local.conf $BUILD_DIR/../{meta-trustx,meta-trustx-intel,meta-trustx-nxp}/conf/multiconfig/*.{conf,inc};do
+for f in  $BUILD_DIR/conf/local.conf $BUILD_DIR/../{meta-gyroidos,meta-gyroidos-intel,meta-gyroidos-nxp,meta-gyroidos-rpi}/conf/multiconfig/*.{conf,inc};do
 	echo "Patching DISTRO in $f"
 	sed -i 's/DISTRO = "core-ref"/DISTRO = "cml-debug"/' $f || true
 	sed -i 's/DISTRO = "cml-base"/DISTRO = "cml-debug-tiny"/' $f || true
